b78b006aad902516dccffd3f66e1234628648e6f
leonardoForaneo <leonardomartinez.contact@gmail.com>
2021-07-10 23:04:00 -0500

    PIPES: registro de idiomas, personalizados (capitalizado y domseguro)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Capitan América';
+  nombre2: string = 'capitán américa';
+  nombre3: string = 'lEonArdo MaRTinEz';
   arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
   porcentaje: number = 0.234;
   salario: number = 1234.5;
   fecha: Date = new Date();
+  idioma: string = 'en';
+  videoUrl: string = 'https://www.youtube.com/embed/1-4Zcr0De9g';
 
   valorPromesa = new Promise<string>((resolve, reject) => {
     setTimeout(() => {
       resolve('Llego la data');
     }, 4500);
   });
+
+  cambiarIdioma(abb: string) {
+    if (abb == 'es') {
+      this.idioma = 'es';
+    } else if (abb == 'en') {
+      this.idioma = 'en';
+    } else {
+      this.idioma = 'fr';
+    }
+  }
 
   heroe = {
     nombre: 'Logan',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CapitalizadoPipe, DomseguroPipe],
   imports: [BrowserModule],
   providers: [
     {
